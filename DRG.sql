fb77cce9c9cc6d456b5ae2f794b04a97f052cf9d
grwna <13523035@std.stei.itb.ac.id>
2025-05-15 19:43:52 +0700

    fix: update sql

--- DRG.sql
+++ DRG.sql
@@ -30,40 +30,40 @@ CREATE TABLE Tier (
         ON UPDATE CASCADE
 );
 
-CREATE TABLE Struk_Langganan (
-    id_supporter INT NOT NULL,
-    id_tier INT NOT NULL,
-    tanggal_mulai DATE,
-    status VARCHAR(50),
-    metode_pembayaran VARCHAR(50),
-    jumlah INT NOT NULL,
-    tanggal_pembayaran_terakhir DATE,
-    CONSTRAINT FOREIGN KEY (id_supporter) REFERENCES Supporter(id_supporter)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    CONSTRAINT FOREIGN KEY (id_tier) REFERENCES tier(id_tier)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-);
+-- CREATE TABLE Struk_Langganan (
+--     id_supporter INT NOT NULL,
+--     id_tier INT NOT NULL,
+--     tanggal_mulai DATE,
+--     status VARCHAR(50),
+--     metode_pembayaran VARCHAR(50),
+--     jumlah INT NOT NULL,
+--     tanggal_pembayaran_terakhir DATE,
+--     CONSTRAINT FOREIGN KEY (id_supporter) REFERENCES Supporter(id_supporter)
+--         ON DELETE CASCADE
+--         ON UPDATE CASCADE,
+--     CONSTRAINT FOREIGN KEY (id_tier) REFERENCES tier(id_tier)
+--         ON DELETE CASCADE
+--         ON UPDATE CASCADE
+-- );
 
 
 
 
-CREATE TABLE Special_Content (
-    id_supporter INT PRIMARY KEY,
-    nama VARCHAR(50),
-    email CHAR(50),
-    alamat VARCHAR(75),
-    tanggal_bergabung DATE
-);
+-- CREATE TABLE Special_Content (
+--     id_supporter INT PRIMARY KEY,
+--     nama VARCHAR(50),
+--     email CHAR(50),
+--     alamat VARCHAR(75),
+--     tanggal_bergabung DATE
+-- );
 
-CREATE TABLE Komentar (
-    id_supporter INT PRIMARY KEY,
-    nama VARCHAR(50),
-    email CHAR(50),
-    alamat VARCHAR(75),
-    tanggal_bergabung DATE
-);
+-- CREATE TABLE Komentar (
+--     id_supporter INT PRIMARY KEY,
+--     nama VARCHAR(50),
+--     email CHAR(50),
+--     alamat VARCHAR(75),
+--     tanggal_bergabung DATE
+-- );
 
 CREATE TABLE Konten (
     id_supporter INT PRIMARY KEY,
@@ -73,83 +73,83 @@ CREATE TABLE Konten (
     tanggal_bergabung DATE
 );
 
-CREATE TABLE Manfaat (
-    id_supporter INT PRIMARY KEY,
-    nama VARCHAR(50),
-    email CHAR(50),
-    alamat VARCHAR(75),
-    tanggal_bergabung DATE
-);
+-- CREATE TABLE Manfaat (
+--     id_supporter INT PRIMARY KEY,
+--     nama VARCHAR(50),
+--     email CHAR(50),
+--     alamat VARCHAR(75),
+--     tanggal_bergabung DATE
+-- );
 
-CREATE TABLE Publikasi (
-    id_supporter INT PRIMARY KEY,
-    nama VARCHAR(50),
-    email CHAR(50),
-    alamat VARCHAR(75),
-    tanggal_bergabung DATE
-);
+-- CREATE TABLE Publikasi (
+--     id_supporter INT PRIMARY KEY,
+--     nama VARCHAR(50),
+--     email CHAR(50),
+--     alamat VARCHAR(75),
+--     tanggal_bergabung DATE
+-- );
 
-CREATE TABLE Inspirasi(
-    id_publikasi INT NOT NULL,
-    id_merchandise INT NOT NULL,
-    FOREIGN KEY (id_publikasi) REFERENCES Publikasi(id_publikasi)
-    FOREIGN KEY (id_merchandise) REFERENCES Merchandise(id_merchandise)
-);
+-- CREATE TABLE Inspirasi(
+--     id_publikasi INT NOT NULL,
+--     id_merchandise INT NOT NULL,
+--     FOREIGN KEY (id_publikasi) REFERENCES Publikasi(id_publikasi)
+--     FOREIGN KEY (id_merchandise) REFERENCES Merchandise(id_merchandise)
+-- );
 
-CREATE TABLE Merchandise(
-    id_merchandise INT PRIMARY KEY,
-    nama VARCHAR(50),
-    harga INT NOT NULL,
-    stok INT NOT NULL,
-    deskripsi VARCHAR(75)
-);
+-- CREATE TABLE Merchandise(
+--     id_merchandise INT PRIMARY KEY,
+--     nama VARCHAR(50),
+--     harga INT NOT NULL,
+--     stok INT NOT NULL,
+--     deskripsi VARCHAR(75)
+-- );
 
-CREATE TABLE Struk_Pembelian(
-    id_supporter INT NOT NULL,
-    id_merchandise INT NOT NULL,
-    jumlah INT NOT NULL,
-    tanggal_pembelian DATE,
-    total_harga INT NOT NULL,
-    metode_pembayaran VARCHAR(75),
-    FOREIGN KEY (id_supporterr) REFERENCES Supporter(id_supporter)
-    FOREIGN KEY (id_merchandise) REFERENCES Merchandise(id_merchandise)
+-- CREATE TABLE Struk_Pembelian(
+--     id_supporter INT NOT NULL,
+--     id_merchandise INT NOT NULL,
+--     jumlah INT NOT NULL,
+--     tanggal_pembelian DATE,
+--     total_harga INT NOT NULL,
+--     metode_pembayaran VARCHAR(75),
+--     FOREIGN KEY (id_supporterr) REFERENCES Supporter(id_supporter)
+--     FOREIGN KEY (id_merchandise) REFERENCES Merchandise(id_merchandise)
 
-);
+-- );
 
-CREATE TABLE Informasi_Hasil (
-    id_gambar INT,
-    id_special INT,
-    status_pengerjaan VARCHAR(75),
-    tanggal_penyelesaian DATE, 
-    feedback VARCHAR(75)
-    FOREIGN KEY (id_gambar) REFERENCES Gambar(id_gambar)
-    FOREIGN KEY (id_special) REFERENCES Special_Content(id_special)
-);
+-- CREATE TABLE Informasi_Hasil (
+--     id_gambar INT,
+--     id_special INT,
+--     status_pengerjaan VARCHAR(75),
+--     tanggal_penyelesaian DATE, 
+--     feedback VARCHAR(75)
+--     FOREIGN KEY (id_gambar) REFERENCES Gambar(id_gambar)
+--     FOREIGN KEY (id_special) REFERENCES Special_Content(id_special)
+-- );
 
 CREATE TABLE Gambar (
     id_gambar INT PRIMARY KEY,
-    id_konten INT,
+    id_konten INT NOT NULL,
     format VARCHAR(75),
     resolusi VARCHAR(75), 
     FOREIGN KEY (id_konten) REFERENCES Konten(id_konten)
 );
 
 CREATE TABLE Teks (
-    id_konten INT,
-    jumlah_kata INT,
+    id_konten INT NOT NULL,
+    jumlah_kata INT NOT NULL,
     format VARCHAR(75),
     FOREIGN KEY (id_konten) REFERENCES Konten(id_konten)
 );
 
 CREATE TABLE Video (
-    id_konten INT,
+    id_konten INT NOT NULL,
     durasi TIME,
     resolusi VARCHAR(75),
     FOREIGN KEY (id_konten) REFERENCES Konten(id_konten)
 );
 
 CREATE TABLE Audio (
-    id_konten INT,
+    id_konten INT NOT NULL,
     durasi TIME,
     kualitas VARCHAR(75) ,
     FOREIGN KEY (id_konten) REFERENCES Konten(id_konten)
